7536a1c514bf62eac285beb6ccbc0e1192f8affa
adi-suresh01 <152752311+adi-suresh01@users.noreply.github.com>
2024-12-11 00:12:43 -0500

    Update main.py

--- main.py
+++ main.py
@@ -50,16 +50,16 @@ def load_model():
     device = get_device()
     model = JEPA(state_latent_dim=state_latent_dim, action_latent_dim=action_latent_dim, hidden_dim=hidden_dim).to(device)
     #model.load_state_dict(torch.load('best_model.pth', weights_only=True))
-    checkpoint_path = "best_model.pth"
-    checkpoint = torch.load(checkpoint_path, map_location=device)
+    # checkpoint_path = "best_model.pth"
+    # checkpoint = torch.load(checkpoint_path, map_location=device)
 
     # Extract only the model state_dict from the checkpoint
-    model_state_dict = checkpoint['model_state_dict']
+    # model_state_dict = checkpoint['model_state_dict']
 
     # Load the model state_dict into your model
-    model.load_state_dict(model_state_dict)
+    # model.load_state_dict(model_state_dict)
 
-    print("Model loaded successfully from checkpoint.")
+    # print("Model loaded successfully from checkpoint.")
     return model
 
 
